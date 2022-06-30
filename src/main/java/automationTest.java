b4cbea60c65ea2ac36688d2d841a03bd3d4799bd
phuong <phuong@gmail.com>
2022-06-30 23:24:13 +0700

    Bai2: web element la gi va thao thac voi cac web element

--- src/main/java/automationTest.java
+++ src/main/java/automationTest.java
@@ -1,5 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,9 +19,78 @@ public class automationTest {
 
   @Test
   public void test01() {
-    System.out.println("hello");
-    chromeDriver.get("http://google.com");
-    sleep(5000);
+    chromeDriver.get("https://auto.fresher.dev/lessons/lession7/index.html");
+
+    sleep(1000);
+    WebElement input = chromeDriver.findElement(By.id("txtInput1"));
+    String value = input.getAttribute("value");
+
+    //    Assert.assertEquals(value, "Default value of input 2222");
+    WebElement btn1 = chromeDriver.findElement(By.id("btnExample1"));
+    //    WebElement btn2 = chromeDriver.findElement(By.className("btn-success"));
+    WebElement text = chromeDriver.findElement(By.id("lbStatusButton"));
+    WebElement textInput = chromeDriver.findElement(By.id("txtInput1"));
+    WebElement select = chromeDriver.findElement(By.id("exampleSelect1"));
+    List<WebElement> listOfOption = chromeDriver.findElements(By.tagName("option"));
+    WebElement dropdownList = chromeDriver.findElement(By.id("exampleSelect1"));
+    WebElement inputCheckBox1 = chromeDriver.findElement(By.id("defaultCheck1"));
+    //        btn1.click();
+    //        sleep(1000);
+
+    //    WebElement btn2 = chromeDriver.findElement(By.className("btn-success"));
+    //    btn2.click();
+    //    sleep(5000);
+
+    List<WebElement> btns = chromeDriver.findElements(By.className("btn-success"));
+
+    //    for (int i = 0; i < btns.size(); i++) {
+    //      btns.get(i).click();
+    //      sleep(2000);
+    //    }
+    //    String textButton = text.getText();
+    //    text.click();
+    //    Assert.assertEquals(textButton, "Click on Button 1", "text wrong");
+    //    textInput.clear();
+    //    sleep(3000);
+    //    textInput.sendKeys("Toi la LA");
+    //    sleep(5000);
+    //    String actualText = textInput.getAttribute("value");
+    //    Assert.assertEquals(actualText, "Toi la LA", "text wrong");
+
+    //    WebElement check1 = chromeDriver.findElement(By.id("defaultCheck1"));
+    //    check1.click();
+    //    sleep(2000);
+
+    //    Select selectElement = new Select(select);
+    //    selectElement.selectByIndex(1);
+    //    sleep(3000);
+    //    selectElement.selectByIndex(2);
+    //    sleep(3000);
+    //    selectElement.selectByValue("value5");
+    //    sleep(3000);
+    //    selectElement.selectByVisibleText("Option 4");
+    //    sleep(3000);
+
+    //    List<String> textOfOptions = new ArrayList<>();
+    //    for (int i = 0; i < listOfOption.size(); i++) {
+    //      String textOption = listOfOption.get(i).getText();
+    //      textOfOptions.add(textOption);
+    //    }
+    //
+    //    for (int i = 0; i < textOfOptions.size(); i++) {
+    //      System.out.println(textOfOptions.get(i));
+    //    }
+
+    inputCheckBox1.click();
+    sleep(3000);
+    boolean isChecked = inputCheckBox1.isSelected();
+    Assert.assertTrue(isChecked);
+
+    // uncheck
+    inputCheckBox1.click();
+    isChecked = inputCheckBox1.isSelected();
+    Assert.assertFalse(isChecked);
+    
   }
 
   @AfterTest
@@ -25,8 +98,8 @@ public class automationTest {
     chromeDriver.close();
   }
 
-  private void sleep(int time){
-    try{
+  private void sleep(int time) {
+    try {
       Thread.sleep(time);
     } catch (InterruptedException e) {
       e.printStackTrace();
